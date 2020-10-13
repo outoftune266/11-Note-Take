e84e2cc20f9618d063e28e6f0a7d1e9b231c7ac9
Zak Monnet <zakmonnet@gmail.com>
2020-10-13 15:59:17 -0500

    added incomplete functions to server.js)

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serves html files
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
@@ -16,7 +17,29 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+// Reads jb.json file and returns saved notes (unfinished)
+// app.get("/api/notes", function(req, res) {
+//     var notes = 
 
+//     return res.json()
+// });
+
+// Posts new notes to database (unfinished)
+// app.post("/api/notes", function(req, res) {
+//     var newNote = req.body;
+//     console.log(newNote);
+
+//     res.json(newNote);
+// })
+
+// Deletes notes (unfinished)
+// app.delete("api/notes/:id", function(req, res) {
+//     var noteID = req.params.id;
+
+//     console.log(noteID);
+
+
+// })
 
 
 app.listen(PORT, function() {
